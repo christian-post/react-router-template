72a0ad9d0e0bb2734324fc70177342b60efc814c
christian-post <chr.post@gmx.net>
2023-08-26 10:16:25 +0200

    router works now xD

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+    createBrowserRouter,
+    RouterProvider,
+  } from "react-router-dom";
 import './index.css'
+
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div>Hello world!</div>,
+    },
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <h1>Hello, World</h1>
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
